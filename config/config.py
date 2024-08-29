7fb46e71427db135012d8e5d8b48c72489e8a682
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-29 11:11:27 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -92,7 +92,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/TheTeamVivek/YukkiMusic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "dev")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv(
